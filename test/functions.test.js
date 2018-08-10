070404956d930de6a24dbf8d50b91884ab5750de
Steven Isbell <steveni_92@msn.com>
2018-08-10 18:46:43 -0500

    updated tests

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,116 +1,109 @@
-const {
-  alternateFilter,
-  alternateForEach,
-  alternateIncludes,
-  alternateIndexOf,
-  alternateMap,
-  alternateReduce
-} = require('../index');
+const { filter, forEach, includes, indexOf, map, reduce } = require('../index');
 
-describe('alternateIncludes', () => {
+describe('includes', () => {
   const arr = [1, 2, 3];
-  test('alternateIncludes should exist', () => {
-    expect(alternateIncludes).toBeDefined();
+  test('includes should exist', () => {
+    expect(includes).toBeDefined();
   });
   test('It should return true if the item is in the target array', () => {
-    expect(arr.alternateIncludes(2)).toBeTruthy();
+    expect(includes(arr, 2)).toBeTruthy();
   });
   test('It should return false if the item is not in the target array', () => {
-    expect(arr.alternateIncludes(5)).toBeFalsy();
+    expect(includes(arr, 5)).toBeFalsy();
   });
 });
 
-describe('alternateIndexOf', () => {
+describe('indexOf', () => {
   const arr = [1, 2, 3];
-  test('alternateIndexOf should exist', () => {
-    expect(alternateIndexOf).toBeDefined();
+  test('indexOf should exist', () => {
+    expect(indexOf).toBeDefined();
   });
   test('It should return the index of the item if the item is in the target array', () => {
-    expect(arr.alternateIndexOf(2)).toBe(1);
+    expect(indexOf(arr, 2)).toBe(1);
   });
   test('It should return -1 if the item is not in the target array', () => {
-    expect(arr.alternateIndexOf(5)).toBe(-1);
+    expect(indexOf(arr, 5)).toBe(-1);
   });
 });
 
-describe('alternateForEach', () => {
+describe('forEach', () => {
   const arr = [1, 2, 3];
   const mock = jest.fn();
-  test('alternateForEach should exist', () => {
-    expect(alternateForEach).toBeDefined();
+  test('forEach should exist', () => {
+    expect(forEach).toBeDefined();
   });
   test('The callback should receive three arguments: the current value, index, and target array', () => {
-    arr.alternateForEach(mock);
+    forEach(arr, mock);
     expect(mock.mock.calls[0][0]).toBe(1);
     expect(mock.mock.calls[0][1]).toBe(0);
     expect(mock.mock.calls[0][2]).toBe(arr);
   });
   test('It should execute the callback for each item in the target array', () => {
-    expect(mock.mock.calls.length).toBe(arr.length);
+    expect(mock.mock.calls.length).toBe(length);
   });
   test('It should return the target array', () => {
-    expect(arr.alternateForEach(mock)).toBe(arr);
+    expect(forEach(arr, mock)).toBe(arr);
   });
 });
 
-describe('alternateMap', () => {
+describe('map', () => {
   const arr = [1, 2, 3];
   const mock = jest.fn();
-  test('alternateMap should exist', () => {
-    expect(alternateMap).toBeDefined();
+  test('map should exist', () => {
+    expect(map).toBeDefined();
   });
   test('The callback should receive three arguments: the current value, index, and target array', () => {
-    arr.alternateMap(mock);
+    map(arr, mock);
     expect(mock.mock.calls[0][0]).toBe(1);
     expect(mock.mock.calls[0][1]).toBe(0);
     expect(mock.mock.calls[0][2]).toBe(arr);
   });
   test('It should execute the callback for each item in the target array', () => {
-    expect(mock.mock.calls.length).toBe(arr.length);
+    expect(mock.mock.calls.length).toBe(length);
   });
   test('It should return the new array', () => {
-    expect(arr.alternateMap(mock)).toHaveLength(arr.length);
-    expect(arr.alternateMap(mock)).toBeInstanceOf(Array);
+    expect(map(arr, mock)).toHaveLength(length);
+    expect(map(arr, mock)).toBeInstanceOf(Array);
   });
 });
 
-describe('alternateFilter', () => {
+describe('filter', () => {
   const arr = [1, 2, 3];
   const mock = jest.fn();
-  test('alternateFilter should exist', () => {
-    expect(alternateFilter).toBeDefined();
+  test('filter should exist', () => {
+    expect(filter).toBeDefined();
   });
   test('The callback should receive three arguments: the current value, index, and target array', () => {
-    arr.alternateFilter(mock);
+    filter(arr, mock);
     expect(mock.mock.calls[0][0]).toBe(1);
     expect(mock.mock.calls[0][1]).toBe(0);
     expect(mock.mock.calls[0][2]).toBe(arr);
   });
   test('It should execute the callback for each item in the target array', () => {
-    expect(mock.mock.calls.length).toBe(arr.length);
+    expect(mock.mock.calls.length).toBe(length);
   });
   test('It should return the new array', () => {
-    expect(arr.alternateFilter(mock)).toBeInstanceOf(Array);
+    expect(filter(arr, mock)).toBeInstanceOf(Array);
   });
 });
 
-describe('alternateReduce', () => {
+describe('reduce', () => {
   const arr = [1, 2, 3];
   const mock = jest.fn();
-  test('alternateReduce should exist', () => {
-    expect(alternateReduce).toBeDefined();
+  test('reduce should exist', () => {
+    expect(reduce).toBeDefined();
   });
   test('The callback should receive four arguments: the accumulator, current value, index, and target array', () => {
-    arr.alternateReduce(mock);
+    reduce(arr, mock);
     expect(mock.mock.calls[0][0]).toBe(0);
     expect(mock.mock.calls[0][1]).toBe(1);
     expect(mock.mock.calls[0][2]).toBe(0);
     expect(mock.mock.calls[0][3]).toBe(arr);
   });
   test('It should execute the callback for each item in the target array', () => {
-    expect(mock.mock.calls.length).toBe(arr.length);
+    expect(mock.mock.calls.length).toBe(length);
   });
   test('It should return the accumulator', () => {
-    expect(arr.alternateReduce((acc, cur) => acc + cur)).toBe(6);
+    expect(reduce(arr, (acc, cur) => acc + cur)).toBe(6);
   });
 });
